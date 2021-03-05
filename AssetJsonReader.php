751456ae578da761c8da5a826124172ab29b72af
kapoko <kasper.koman@gmail.com>
2021-03-05 16:51:24 +0100

    Added readme, updated main class

--- AssetJsonReader.php
+++ AssetJsonReader.php
@@ -2,10 +2,98 @@
 
 namespace Kapoko\AssetJsonReader;
 
+use Symfony\Component\Filesystem\Filesystem;
+
 class AssetJsonReader
 {
-    public function greet($greet = "Hello World")
+    /**
+     * @var array 
+     */
+    protected $manifest;
+
+    /**
+     * @var string 
+     */
+    protected $path;
+    
+    /**
+     * Manifest constructor
+     * 
+     * @return void
+     */
+    public function __construct($path) 
     {
-        return $greet;
+        $this->registerManifest($path);
+    }
+
+    /**
+     * Registers the manifest and parses the values
+     * 
+     * @param  string $path Path to the manifest file
+     * @return void
+     */
+    public function registerManifest($path)
+    {
+        $this->path = $path;
+
+        $manifest = $this->getJsonManifest($this->path);
+
+        foreach ($manifest as $key => $value) {
+            $this->set($key, $value);
+        }
+    }
+
+    /**
+     * Setting key value pair, if array call function recursively
+     * 
+     * @return void
+     */
+    public function set($key, $value): void
+    {
+        if (is_array($value)) {
+            foreach($value as $innerKey => $innerValue) {
+                $this->set($key . '.' . $innerKey, $innerValue);
+            }
+        } else {
+            $this->manifest[$this->normalizeRelativePath($key)] = $this->normalizeRelativePath($value);
+        }
+    }
+
+    /**
+     * Getting value from the manifest by key
+     * 
+     * @return string
+     */
+    public function get($key): string
+    {
+        $key = $this->normalizeRelativePath($key);
+
+        $relative_path = $this->manifest[$key] ?? $key;
+
+        return $relative_path;
+    }
+
+    /**
+     * Normalizes to forward slashes and removes leading slash.
+     *
+     * @return string
+     */
+    protected function normalizeRelativePath(string $path): string
+    {
+        $path = str_replace('\\', '/', $path);
+        return ltrim($path, '/');
+    }
+
+    /**
+     * Get the file from disk and decoded it 
+     * 
+     * @param  string $jsonManifestPath Path to the manifest file
+     * @return array
+     */
+    protected function getJsonManifest(string $jsonManifestPath): array
+    {
+        $filesystem = new Filesystem();
+        
+        return $filesystem->exists($jsonManifestPath) ? \json_decode(file_get_contents($jsonManifestPath), true) : [];
     }
 }
---
+++ README.md
@@ -0,0 +1,37 @@
+# asset-json-reader
+
+Reads asset locations from a json file outputted by webpack. Works well with [asset-webpack-plugin](https://www.npmjs.com/package/assets-webpack-plugin), which stores the hashed filenames in a json file. With this package you can easily get the hashed filenames
+
+## Install
+
+```sh
+composer require kapoko/asset-json-reader
+```
+
+## Usage
+
+Output by the [asset-webpack-plugin](https://www.npmjs.com/package/assets-webpack-plugin) may look something like this (`dist/assets.json`):
+
+```json
+{
+    "one": {
+        "js": "/js/one_2bb80372ebe8047a68d4.bundle.js"
+    },
+    "two": {
+        "js": "/js/two_2bb80372ebe8047a68d4.bundle.js"
+    }
+}
+```
+
+Usage is:
+
+```php
+use Kapoko\AssetJsonReader\AssetJsonReader;
+
+$assets = new AssetJsonReader($pathToJson);
+
+$assets->get('one.js')); // Returns '/js/one_2bb80372ebe8047a68d4.bundle.js'
+$assets->get('two.js')); // Returns '/js/two_2bb80372ebe8047a68d4.bundle.js'
+```
+
+If the manifest doesn't exists it returns the given string, which might be handy in development mode when the files aren't hashed.
